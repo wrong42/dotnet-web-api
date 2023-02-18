f02635ac9c6dc4528f8a334c2fec74673d0e3db7
Ondřej Exner <ondrej.exner@notino.com>
2023-02-18 20:03:03 +0100

    add WebApi project with video controller

---
+++ WebApi/VideoController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+using System.IO;
+
+namespace WebApi;
+
+[ApiController]
+[Route("api/[controller]")]
+public class VideoController : ControllerBase
+{
+    private static string GetVideoName(int videoId)
+    {
+        return videoId switch
+        {
+            1 => "ocean.mp4",
+            2 => "forest.mp4",
+            3 => "rocks.mp4",
+            _ => throw new ArgumentOutOfRangeException(),
+        };
+    }
+
+    [HttpGet("{id:int}")]
+    public FileStreamResult GetVideo([FromRoute]int id)
+    {
+        var videoName = GetVideoName(id);
+
+        var videoPath = @"C:\Users\ondrej.exner\Downloads\{0}";
+        var videoStream = new FileStream(string.Format(videoPath, videoName), FileMode.Open);
+
+        var response = new FileStreamResult(videoStream, new MediaTypeHeaderValue("video/mp4"))
+        {
+            FileDownloadName = videoName,
+        };
+
+        return response;
+    }
+}
